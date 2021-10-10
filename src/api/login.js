0451230923f05d75378b6a26de0ab0ecc461eefc
Jesslits <75296205+Css22@users.noreply.github.com>
2021-10-10 15:57:51 +0800

    Update login.js

--- src/api/login.js
+++ src/api/login.js
@@ -1,8 +1,7 @@
 import request from "@/utils/request";
-
 export function login(username, password) {
   return request({
-    url: "/admin/login",
+    url: "/uaa/login",
     method: "post",
     data: {
       username,
@@ -11,70 +10,12 @@ export function login(username, password) {
   });
 }
 
-export function getInfo() {
+export function logout(Token) {
   return request({
-    url: "/admin/info",
-    method: "get",
-  });
-}
-
-export function logout() {
-  return request({
-    url: "/admin/logout",
+    url: "/uaa/account",
     method: "post",
-  });
-}
-
-export function fetchList(params) {
-  return request({
-    url: "/admin/list",
-    method: "get",
-    params: params,
-  });
-}
-
-export function createAdmin(data) {
-  return request({
-    url: "/admin/register",
-    method: "post",
-    data: data,
-  });
-}
-
-export function updateAdmin(id, data) {
-  return request({
-    url: "/admin/update/" + id,
-    method: "post",
-    data: data,
-  });
-}
-
-export function updateStatus(id, params) {
-  return request({
-    url: "/admin/updateStatus/" + id,
-    method: "post",
-    params: params,
-  });
-}
-
-export function deleteAdmin(id) {
-  return request({
-    url: "/admin/delete/" + id,
-    method: "post",
-  });
-}
-
-export function getRoleByAdmin(id) {
-  return request({
-    url: "/admin/role/" + id,
-    method: "get",
-  });
-}
-
-export function allocRole(data) {
-  return request({
-    url: "/admin/role/update",
-    method: "post",
-    data: data,
+    data:{
+      Token
+    }
   });
 }
