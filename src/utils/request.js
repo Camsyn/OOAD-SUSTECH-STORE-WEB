4830e35e8386e698d3e711cc8e81a8127c557631
Jesslits <75296205+Css22@users.noreply.github.com>
2021-10-10 15:58:33 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,72 +1,36 @@
 import axios from "axios";
-// import { Message, MessageBox } from "element-ui";
 import store from "../store";
 import { getToken } from "@/utils/auth";
 
-// 创建axios实例
 const service = axios.create({
-  baseURL: "/api", // api的base_url
-  timeout: 15000, // 请求超时时间
+  baseURL: "camsyn.cn:8000/auth",
+  timeout: 5000,
 });
-
-// request拦截器
+//发送请求前
 service.interceptors.request.use(
   (config) => {
     if (store.getters.token) {
-      config.headers["Authorization"] = getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
+      config.headers["Authorization"] = getToken(); // 更新token
     }
     return config;
   },
   (error) => {
-    // Do something with request error
-    console.log(error); // for debug
     return Promise.reject(error);
   }
 );
 
-// respone拦截器
+//发送请求后
 service.interceptors.response.use(
-  (response) => {
-    /**
-     * code为非200是抛错 可结合自己业务进行修改
-     */
+   (response) => {
     const res = response.data;
-    if (res.code !== 200) {
-      // Message({
-      //   message: res.message,
-      //   type: "error",
-      //   duration: 3 * 1000,
-      // });
-
-      // 401:未登录;
-      if (res.code === 401) {
-        // MessageBox.confirm(
-        //   "你已被登出，可以取消继续留在该页面，或者重新登录",
-        //   "确定登出",
-        //   {
-        //     confirmButtonText: "重新登录",
-        //     cancelButtonText: "取消",
-        //     type: "warning",
-        //   }
-        // ).then(() => {
-        //   store.dispatch("FedLogOut").then(() => {
-        //     location.reload(); // 为了重新实例化vue-router对象 避免bug
-        //   });
-        // });
-      }
-      return Promise.reject("error");
+    if (res.code !== 0) { // 判断错误码
+      return Promise.reject(res.code); //直接返回错误码
     } else {
-      return response.data;
+      return response.data; // 请求成功 返回数据
     }
   },
   (error) => {
-    console.log("err" + error); // for debug
-    // Message({
-    //   message: error.message,
-    //   type: "error",
-    //   duration: 3 * 1000,
-    // });
-    return Promise.reject(error);
+    return Promise.reject(error); //直接返回错误码
   }
 );
 
