8f4339d7562605eec47171f22046784c6de29503
ETLAN <965851275@qq.com>
2021-12-28 23:31:08 +0800

    new version2

--- src/components/Myspace/MySpace.vue
+++ src/components/Myspace/MySpace.vue
@@ -40,7 +40,10 @@
               </v-tabs>
             </v-col>
           </v-row>
-          <router-view></router-view>
+          <transition name="fade">
+            <router-view></router-view>
+          </transition>
+
         </v-sheet>
       </v-col>
     </v-row>
@@ -74,4 +77,11 @@ export default {
 </script>
 
 <style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
