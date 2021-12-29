4e7f0a8f55d731a21172cdb65f0eb392af7a61d6
Killer_Queen <11910620@mail.sustech.edu.cn>
2021-12-29 15:38:04 +0800

    api

--- src/api/review.js
+++ src/api/review.js
@@ -4,7 +4,7 @@ import service from "../utils/request";
 
 export function selectReportRecord(i_id) {
   return service({
-      url: "/review/review/selectReportRecord",
+      url: "/review/selectReportRecord",
       method: "get",
       params: {i_id},
   });
@@ -12,7 +12,7 @@ export function selectReportRecord(i_id) {
 
 export function selectReportsAboutUser(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutUser",
+        url: "/review/selectReportsAboutUser",
         method: "get",
         params:{t_id},
     });
@@ -20,7 +20,7 @@ export function selectReportsAboutUser(t_id) {
 
 export function selectReportsAboutRequest(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutRequest",
+        url: "/review/selectReportsAboutRequest",
         method: "get",
         params:{t_id},
     });
@@ -28,7 +28,7 @@ export function selectReportsAboutRequest(t_id) {
 
 export function selectReportsAboutOrder(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutOrder",
+        url: "/review/selectReportsAboutOrder",
         method: "get",
         params:{t_id},
     });
@@ -36,7 +36,7 @@ export function selectReportsAboutOrder(t_id) {
 
 export function selectReportsAboutChat(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutChat",
+        url: "/review/selectReportsAboutChat",
         method: "get",
         params:{t_id},
     });
@@ -44,7 +44,7 @@ export function selectReportsAboutChat(t_id) {
 
 export function selectReportsAboutCircle(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutCircle",
+        url: "/review/selectReportsAboutCircle",
         method: "get",
         params:{t_id},
     });
@@ -52,7 +52,7 @@ export function selectReportsAboutCircle(t_id) {
 
 export function selectReportsAboutComment(t_id) {
     return service({
-        url: "/review/review/selectReportsAboutComment",
+        url: "/review/selectReportsAboutComment",
         method: "get",
         params: {t_id},
     });
@@ -60,7 +60,7 @@ export function selectReportsAboutComment(t_id) {
 
 export function selectSendingAboutUser(t_id) {
     return service({
-        url: "/review/review/selectSendingAboutUser",
+        url: "/review/selectSendingAboutUser",
         method: "get",
         params: {t_id},
     });
@@ -68,7 +68,7 @@ export function selectSendingAboutUser(t_id) {
 
 export function selectSendingAboutRequest(t_id) {
     return service({
-        url: "/review/review/selectSendingAboutRequest",
+        url: "/review/selectSendingAboutRequest",
         method: "get",
         params: {t_id},
     });
@@ -76,7 +76,7 @@ export function selectSendingAboutRequest(t_id) {
 
 export function selectSendingAboutOrder(t_id) {
     return service({
-        url: "/review/review/selectSendingAboutOrder",
+        url: "/review/selectSendingAboutOrder",
         method: "get",
         params:{t_id},
     });
@@ -84,7 +84,7 @@ export function selectSendingAboutOrder(t_id) {
 
 export function selectSendingAboutChat (t_id) {
     return service({
-        url: "/review/review/selectSendingAboutChat",
+        url: "/review/selectSendingAboutChat",
         method: "get",
         params:{t_id},
     });
@@ -92,7 +92,7 @@ export function selectSendingAboutChat (t_id) {
 
 export function selectSendingAboutCircle (t_id) {
     return service({
-        url: "/review/review/selectSendingAboutCircle",
+        url: "/review/selectSendingAboutCircle",
         method: "get",
         params:{t_id},
     });
@@ -100,7 +100,7 @@ export function selectSendingAboutCircle (t_id) {
 
 export function selectSendingAboutComment (t_id) {
     return service({
-        url: "/review/review/selectSendingAboutComment",
+        url: "/review/selectSendingAboutComment",
         method: "get",
         params:{t_id},
     });
@@ -110,7 +110,7 @@ export function selectSendingAboutComment (t_id) {
 
 export function reportCircle(data) {
     return service({
-        url: "/review/review/reportCircle",
+        url: "/review/reportCircle",
         method: "post",
         params: data,
     });
@@ -118,7 +118,7 @@ export function reportCircle(data) {
 
 export function reportComment(t_id, desc) {
     return request({
-        url: "/review/review/reportComment",
+        url: "/review/reportComment",
         method: "post",
         params: {t_id, desc},
     });
@@ -126,7 +126,7 @@ export function reportComment(t_id, desc) {
 
 export function reportUser(t_id, desc) {
     return service({
-        url: "/review/review/reportUser",
+        url: "/review/reportUser",
         method: "post",
         params: {t_id, desc},
     });
@@ -134,7 +134,7 @@ export function reportUser(t_id, desc) {
 
 export function reportRequest(t_id, desc) {
     return service({
-        url: "/review/review/reportRequest",
+        url: "/review/reportRequest",
         method: "post",
         params: {t_id, desc},
     });
@@ -142,7 +142,7 @@ export function reportRequest(t_id, desc) {
 
 export function reportOrder(t_id, desc) {
     return service({
-        url: "/review/review/reportOrder",
+        url: "/review/reportOrder",
         method: "post",
         params: {t_id, desc},
     });
@@ -150,7 +150,7 @@ export function reportOrder(t_id, desc) {
 
 export function reportChat(t_id, desc) {
     return service({
-        url: "/review/review/reportChat",
+        url: "/review/reportChat",
         method: "post",
         params: {t_id, desc},
     });
@@ -158,7 +158,7 @@ export function reportChat(t_id, desc) {
 
 export function Argue(R_id, desc) {
     return service({
-        url: "/review/review/argue",
+        url: "/review/argue",
         method: "put",
         data: {R_id, desc},
     });
